370bdd0bc3fa644187de224b5948f912e943b2c0
박원균 <dlsrk489@gmail.com>
2022-10-25 20:43:32 +0900

    #4 잡플래닛 채용공고 파싱을 위한 소스코드 추가

---
+++ setting.sh
@@ -0,0 +1,3 @@
+chromeVersion=$(ls /mnt/c/Program\ Files/Google/Chrome/Application | grep -E '[0-9].[0-9].[0-9].[0-9]')
+echo "$chromeVersion <-- 이 버전을 다운로드 하시면 됩니다."
+echo "https://chromedriver.chromium.org/downloads"
