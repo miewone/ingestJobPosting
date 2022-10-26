5f87e591b2db36ed0633cead7b547b5879306860
박원균 <dlsrk489@gmail.com>
2022-10-27 00:23:57 +0900

    [Alter] word cloud 컴포넌트화 (#5)

---
+++ front/src/components/WordCloud.js
@@ -0,0 +1,48 @@
+import React, { useState } from 'react';
+import ReactWordcloud from "react-wordcloud";
+import { Resizable } from "re-resizable";
+
+
+const options = {
+    colors: ["#4E197F", "#2A63B3", "#4FB35D", "#E19D23", "#CC3A38"],
+    enableTooltip: false,
+    deterministic: false,
+    fontFamily: "Lora",
+    fontSizes: [10, 40],
+    fontStyles: "normal",
+    fontWeight: "700",
+    padding: 1,
+    rotations: 1,
+    rotationAngles: [0, 90],
+    scale: "liner",
+    spiral: "archimedean",
+    transitionDuration: 1000
+};
+
+const resizeStyle = {
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center",
+    border: "solid 1px #ddd"
+};
+const WordCloud = ({source}) => {
+    console.log(source)
+    return (
+        <div>
+            <Resizable
+                defaultSize={{
+                    width: 500,
+                    height: 250
+                }}
+                style={resizeStyle}
+
+            >
+                <div style={{ width: "100%", height: "100%" }}>
+                    <ReactWordcloud words={source} options={options} />
+                </div>
+            </Resizable>
+        </div>
+    );
+};
+
+export default WordCloud;
