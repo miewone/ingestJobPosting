ae5382eaa482f976423afd6e91914d043c9380b5
박원균 <dlsrk489@gmail.com>
2022-10-27 00:22:21 +0900

    react public 소스들 추가 (#5)

--- front/src/App.js
+++ front/src/App.js
@@ -1,45 +1,23 @@
-import ReactWordcloud from "react-wordcloud";
-import { Resizable } from "re-resizable";
+import { useState } from "react";
+import WordCloud from "./components/WordCloud";
 import words from "./components/words";
-
-const options = {
-  colors: ["#4E197F", "#2A63B3", "#4FB35D", "#E19D23", "#CC3A38"],
-  enableTooltip: false,
-  deterministic: false,
-  fontFamily: "Lora",
-  fontSizes: [10, 40],
-  fontStyles: "normal",
-  fontWeight: "700",
-  padding: 1,
-  rotations: 1,
-  rotationAngles: [0, 90],
-  scale: "liner",
-  spiral: "archimedean",
-  transitionDuration: 1000
-};
-
-const resizeStyle = {
-  display: "flex",
-  alignItems: "center",
-  justifyContent: "center",
-  border: "solid 1px #ddd"
-};
-
+import words2 from "./components/words2";
+import  {ReactComponent  as Kr} from './assets/kr.svg'
 function App() {
+
+  const [sousrce,setSource] = useState([]);
   return (
     <div >
-      <Resizable
-        defaultSize={{
-          width: 500,
-          height: 250
-        }}
-        style={resizeStyle}
-
-      >
-        <div style={{ width: "100%", height: "100%" }}>
-          <ReactWordcloud words={words} options={options} />
-        </div>
-      </Resizable>
+      <label>
+        <input type="radio" value="1" name="test1" onClick={() => setSource(words)}/>
+        test
+      </label>
+      <label>
+        <input type="radio" value="2" name="test1" onClick={() => setSource(words2)}/>
+        test2
+      </label>
+      <WordCloud source={sousrce} />
+      <Kr />
     </div>
   );
 }
