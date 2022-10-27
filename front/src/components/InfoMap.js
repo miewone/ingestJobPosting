a2421ccd3a9251da42ff4a4cff886b5ba7c3f165
박원균 <dlsrk489@gmail.com>
2022-10-27 17:58:33 +0900

    [Update] svg get attribute Name, Id (#11)

--- front/src/components/InfoMap.js
+++ front/src/components/InfoMap.js
@@ -5,14 +5,17 @@ import { ReactComponent as Kr } from '../assets/kr.svg'
 
 const InfoMap = () => {
     const [locations, setLocations] = useState([]);
-    const testRef = useRef()
+    const testRef = useRef();
     useEffect( ()=> {
-        setLocations(Object.entries(testRef.current.childNodes).map((val) => {
-            return val[1].id
-        }))
-    },[])
-    console.log(testRef.current.childNodes)
-    console.log(locations)
+        setLocations(Object.entries(testRef.current.childNodes)
+            .filter(node => node[1].nodeName == "path")
+            .map((val) => {
+                console.log(val[1]);
+                return {name : val[1].attributes.name.value , id :val[1].id};
+            }))
+        
+    },[]);
+    console.log(locations);
     return (
         <div>
             <Kr onClick={(e) => {
