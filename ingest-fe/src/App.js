3fd60b1c780e2b07e20548e9a9d523d5c8c09d69
ë <dlsrk489@gmail.com>
2022-10-31 01:22:26 +0900

    [Update] wordCloud, Map통합 (#8)
    App.js 에서 뿌리던 map 과 wordcloud를 wordcloud 컴포넌트 변화에 따라 map도 변경을 위해 하나의 컴포넌트로 합침
    
    wordcloud에 나타는 글자를 클릭하면 지도에 글자의 값만큼 색으로 강조 기능

--- ingest-fe/src/App.js
+++ ingest-fe/src/App.js
@@ -1,15 +1,37 @@
-import React, { useState } from 'react';
-import InfoMap from "./components/InfoMap";
+import React, { useState, useEffect, useRef } from 'react';
+
 import WordCloud from "./components/WordCloud";
 import words from "./components/words";
 import words2 from "./components/words2";
+import axios from "axios";
+
+const Counter = (array) => {
+    var count = {};
+    array.forEach(val => count[val] = (count[val] || 0) + 1);
+    return count;
+}
+
+let jobpostingSkills = []
 
 const App = () => {
     const [sousrce, setSource] = useState([]);
+    useEffect(() => {
+        const apiGetSkills = async () => {
+            await axios.get("/jobinfo/skills")
+                .then((res) => {
+                    jobpostingSkills = Object.entries(Counter(res.data)).map(values => {return {text : values[0].toUpperCase()  ,value : Math.log2(values[1]) * 10}});
+                    setSource(jobpostingSkills);
+                })
+                .catch((err) => {
+                    console.log(err);
+                })
+        };
+        sousrce != undefined ? apiGetSkills() : sousrce
+    }, [])
     return (
         <div >
             <label>
-                <input type="radio" value="1" name="test1" onClick={() => setSource(words)} />
+                <input type="radio" value="1" name="test1" onClick={() => setSource(jobpostingSkills)} />
                 test
             </label>
             <label>
@@ -17,7 +39,7 @@ const App = () => {
                 test2
             </label>
             <WordCloud source={sousrce} />
-            <InfoMap />
+            
         </div>
     );
 };
