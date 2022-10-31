769ce2396ee7190917397b000565bff2c6332ed6
박원균 <dlsrk489@gmail.com>
2022-10-31 18:15:46 +0900

    [Update] word cloud 기능 변경 (#11)
    글자 이벤트가 발생할때 리렌더링 disable
    worcloud 맥스값 지정

--- ingest-fe/src/components/WordCloud.js
+++ ingest-fe/src/components/WordCloud.js
@@ -1,10 +1,9 @@
-import React, { useState, useEffect,useRef } from 'react';
+import React, { useState, useEffect,useRef, memo } from 'react';
 import ReactWordcloud from "react-wordcloud";
 import { Resizable } from "re-resizable";
 import axios from 'axios';
-import InfoMap from "./InfoMap";
 import Mapkr  from "../assets/Mapkr"
-
+import { select } from "d3-selection";
 
 const KR_LOCATION = ["충청북도", "인천", "강원", "서울", "경기", "전라북도", "광주", "충천남도", "대전", "대구", "경상남도", "전라남도", "부산", "울산", "경상북도", "제주도", "세종"]
 const findLocationInlocation = (str) => {
@@ -30,20 +29,23 @@ const resizeStyle = {
     justifyContent: "center",
     border: "solid 1px #ddd"
 };
+const size = [600, 400];
+
 let locationz = "";
 const WordCloud = ({ source }) => {
+    const [pocusloations,setPocuslocations] = useState('');
     const [locations, setLocations] = useState([]);
     const [getMaplocations, setGetMapLocations] = useState([]);
 
     const options = {
-        colors: ["#4E197F", "#2A63B3", "#4FB35D", "#E19D23", "#CC3A38"],
+        colors: ["#4E197F", "#2A63B3", "#4FB35D"],
         enableTooltip: false,
-        deterministic: false,
+        deterministic: true,
         fontFamily: "Lora",
         fontSizes: [10, 40],
         fontStyles: "normal",
         fontWeight: "700",
-        padding: 1,
+        padding: 1.5,
         rotations: 1,
         rotationAngles: [0, 90],
         scale: "liner",
@@ -52,14 +54,11 @@ const WordCloud = ({ source }) => {
     };
 
     const searchLocationBySkill = async (e) => {
-        let resDataLocations = []
         CounterKrLocations = Counter(KR_LOCATION);
         await axios.get(`/jobinfo/locations/${e.text}`)
             .then((res) => {
-                // console.log("클릭 데이터")
-                // console.log(res.data)
-                // // locationz = res.data
-                // console.log(typeof(res.data))
+                console.log(e);
+                setPocuslocations(e.text);
                 const data = res.data.map(value => {
                     findLocationInlocation(value)
                 })
@@ -67,14 +66,29 @@ const WordCloud = ({ source }) => {
             })
             .catch((err) => {
                 console.log(err);
-            })
-
+            });
     }
+    function getCallback(callback) {
+        return function (word, event) {
+        
+          const isActive = callback !== "onWordMouseOut";
+          const element = event.target;
+          const text = select(element);
+          const multiSize = 1.5
+          console.log(text);
+          const textOriginfontSzie =text.attr("font-size").split("px")[0];
+          text
+            .transition()
+            .attr("background", "black");
+            // .attr("font-size", isActive ? textOriginfontSzie*multiSize+"px" : textOriginfontSzie/multiSize+"px");
+        };
+      }
 
     const callbacks = {
         // getWordColor: word => word.value > 50 ? "blue" : "red",
         onWordClick: searchLocationBySkill,
-        // onWordMouseOver: console.log,
+        // onWordMouseOver: searchLocationBySkill,
+        // onWordMouseOut: getCallback("onWordMouseOut"),
         // getWordTooltip: word => `${word.text} (${word.value}) [${word.value > 50 ? "good" : "bad"}]`,
     }
 
@@ -85,8 +99,9 @@ const WordCloud = ({ source }) => {
             .map((val) => {
                 return { name: val[1].attributes.name.value, id: val[1].id };
             }));
-    }, [locations]);
+    }, []);
     return (
+        // <div style={{ display: 'flex'}}>
         <div style={{ display: 'flex'}}>
             {/* <Resizable
                 defaultSize={{
@@ -96,9 +111,18 @@ const WordCloud = ({ source }) => {
                 style={resizeStyle}
 
             > */}
-                <div style={{marginTop:"5em", width: "50%", height: "100%"}}>
-                    <ReactWordcloud callbacks={callbacks} words={source} options={options} />
-                </div>
+            <div style={{position:"absolute", left:"50%"}}>
+                {pocusloations}
+            </div>
+            <div style={{marginTop:"5em", width: "50%", height: "100%"}}>
+                <ReactWordcloud 
+                    callbacks={callbacks}
+                    words={source}
+                    options={options}
+                    maxWords={150} 
+                    // size={size}
+                />
+            </div>
             {/* </Resizable> */}
             {/* <InfoMap /> */}
             <div style={{ width: "50%", height: "100%" }}>
