bdc80d38fbc3b68d47fdc3a9a9017ef8897310d2
박원균 <dlsrk489@gmail.com>
2022-11-02 00:56:42 +0900

    [Update] 소스 리팩토링 (#11)
    app.js 에서 호출하여 props로 전달이 굳이 필요하지 안항 wordcloud.js에 통합하여 관리하도록 하였습니다.

--- ingest-fe/src/App.js
+++ ingest-fe/src/App.js
@@ -1,45 +1,13 @@
-import React, { useState, useEffect, useRef } from 'react';
+import React from 'react';
 
 import WordCloud from "./components/WordCloud";
-import words from "./components/words";
-import words2 from "./components/words2";
-import axios from "axios";
 
-const Counter = (array) => {
-    var count = {};
-    array.forEach(val => count[val] = (count[val] || 0) + 1);
-    return count;
-}
 
-let jobpostingSkills = []
 
 const App = () => {
-    const [sousrce, setSource] = useState([]);
-    useEffect(() => {
-        const apiGetSkills = async () => {
-            await axios.get("/jobinfo/skills")
-                .then((res) => {
-                    jobpostingSkills = Object.entries(Counter(res.data)).map(values => {return {text : values[0].toUpperCase()  ,value : Math.log2(values[1]) * 10}});
-                    setSource(jobpostingSkills);
-                })
-                .catch((err) => {
-                    console.log(err);
-                })
-        };
-        sousrce != undefined ? apiGetSkills() : sousrce
-    }, [])
     return (
         <div >
-            <label>
-                <input type="radio" value="1" name="test1" onClick={() => setSource(jobpostingSkills)} />
-                test
-            </label>
-            <label>
-                <input type="radio" value="2" name="test1" onClick={() => setSource(words2)} />
-                test2
-            </label>
-            <WordCloud source={sousrce} />
-            
+            <WordCloud />
         </div>
     );
 };
--- ingest-fe/src/components/WordCloud.js
+++ ingest-fe/src/components/WordCloud.js
@@ -1,33 +1,44 @@
-import React, { useState, useEffect,useRef, memo } from 'react';
+import React, { useState, useEffect, useRef, memo } from 'react';
 import ReactWordcloud from "react-wordcloud";
 import { Resizable } from "re-resizable";
 import axios from 'axios';
-import Mapkr  from "../assets/Mapkr"
+import Mapkr from "../assets/Mapkr"
 import { select } from "d3-selection";
+import "../assets/style/wordcloud.css";
 
-let CounterKrLocations = ""
+let CounterKrLocations = "";
+let isActive = false;
+const Counter = (array) => {
+    var count = {};
+    array.forEach(val => count[val] = (count[val] || 0) + 1);
+    return count;
+};
 
+const options = {
+    colors: ["#4E197F", "#2A63B3", "#4FB35D"],
+    enableTooltip: false,
+    deterministic: true,
+    fontFamily: "Lora",
+    fontSizes: [10, 40],
+    fontStyles: "normal",
+    fontWeight: "700",
+    padding: 1.5,
+    rotations: 1,
+    rotationAngles: [0, 90],
+    scale: "liner",
+    spiral: "archimedean",
+    transitionDuration: 1000
+};
 
-const WordCloud = ({ source }) => {
-    const [pocusloations,setPocuslocations] = useState('');
+let jobpostingSkills = [];
+
+const WordCloud = () => {
+    const [source, setSource] = useState([]);
     const [locations, setLocations] = useState([]);
+    const testRef = useRef();
+    // const [allLocations,setAllLocations] = useState({});
 
-
-    const options = {
-        colors: ["#4E197F", "#2A63B3", "#4FB35D"],
-        enableTooltip: false,
-        deterministic: true,
-        fontFamily: "Lora",
-        fontSizes: [10, 40],
-        fontStyles: "normal",
-        fontWeight: "700",
-        padding: 1.5,
-        rotations: 1,
-        rotationAngles: [0, 90],
-        scale: "liner",
-        spiral: "archimedean",
-        transitionDuration: 1000
-    };
+    
 
     const searchLocationBySkill = async (e) => {
         CounterKrLocations = ""
@@ -38,63 +49,102 @@ const WordCloud = ({ source }) => {
             .catch((err) => {
                 console.log(err);
             });
-    }
-    function getCallback(callback) {
-        return function (word, event) {
-        
-          const isActive = callback !== "onWordMouseOut";
-          const element = event.target;
-          const text = select(element);
-          const multiSize = 1.5
-          console.log(text);
-          const textOriginfontSzie =text.attr("font-size").split("px")[0];
-          text
-            .transition()
-            .attr("background", "black");
-            // .attr("font-size", isActive ? textOriginfontSzie*multiSize+"px" : textOriginfontSzie/multiSize+"px");
-        };
-      }
+    };
+    const getAllSkills = async () => {
+        // isActive = true;
+        setLocations({});
+        await axios.get("/jobinfo/skills")
+            .then((res) => {
+                setSource(Object.entries(Counter(res.data)).map(values => {return {text : values[0].toUpperCase()  ,value : Math.log2(values[1]) * 10}}));
+            })
+            .catch((err) => {
+                console.log(err);
+            });
+    };
+    const getAllLocations = async() =>{
+        // isActive = false;
+        await axios.get('/jobinfo/exposebylocation')
+            .then((res) => {
+                setLocations(res.data);
+            })
+            .catch((err)=>{
+                console.log(err);
+            });
+    };
+    // function getCallback(callback) {
+    //     return function (word, event) {
+
+    //         const isActive = callback !== "onWordMouseOut";
+    //         const element = event.target;
+    //         const text = select(element);
+    //         const multiSize = 1.5
+    //         console.log(text);
+    //         const textOriginfontSzie = text.attr("font-size").split("px")[0];
+    //         text
+    //             .transition()
+    //             .attr("background", "black");
+    //         // .attr("font-size", isActive ? textOriginfontSzie*multiSize+"px" : textOriginfontSzie/multiSize+"px");
+    //     };
+    // }
 
     const callbacks = {
-        // getWordColor: word => word.value > 50 ? "blue" : "red",
         onWordClick: searchLocationBySkill,
-        // onWordMouseOver: searchLocationBySkill,
-        // onWordMouseOut: getCallback("onWordMouseOut"),
-        // getWordTooltip: word => `${word.text} (${word.value}) [${word.value > 50 ? "good" : "bad"}]`,
-    }
+    };
 
-    const testRef = useRef();
+    console.log(source);
     return (
-        <div style={{ display: 'flex'}}>
-            <div style={{position:"absolute", left:"50%"}}>
-                {pocusloations}
-            </div>
-            <div style={{marginTop:"5em", width: "40%", height: "100%"}}>
-                <ReactWordcloud 
-                    callbacks={callbacks}
-                    words={source}
-                    options={options}
-                    maxWords={50} 
+        <div>
+            <label>
+                <input type="radio" value="1" name="test1"  />
+                전체공고 로딩
+            </label>
+            <label>
+                <input type="radio" value="2" name="test1"  onChange={getAllSkills}/>
+                스킬 - 내부에 체크박스로 카테고리 별로 볼 수 있게
+            </label>
+            <label>
+                <input type="radio" value="2" name="test1"  onChange={getAllLocations} />
+                지역
+            </label>
+            <label>
+                <input type="radio" value="2" name="test1"  />
+                복지
+            </label>
+            <div  style={{ display: 'flex' }}>
+                {/* <div>
+                    표시될 단어 수<input type="text" />
+                </div>
+                <div style={{ position: "absolute", left: "50%" }}>
+                    {pocusloations}
+                </div> */}
+                <div style={{ marginTop: "5em", width: "40%", height: "100%" }}>
+                    {<ReactWordcloud
+                        callbacks={callbacks}
+                        words={source}
+                        options={options}
+                        maxWords={100} // TODO : 최대 노출 문자 지정하게 끔 기능 구현.
                     // size={size}
-                />
+                    />}
+                </div>
+                <div style={{ width: "50%", height: "100%" }}>
+                    <Mapkr
+                        click={(e) => { console.log(e.target[Object.keys(e.target)[1]].id) }}
+                        locations={locations}
+                        ref={testRef}
+                    />
+                </div>
+                <div style={{ display: 'flex', flexDirection: 'column', width: "10%", position: 'absolute', right: '1em' }}>
+                    {Object.entries(locations).map((value, idx) => {
+                        return (
+                            value[0].length > 0 && value[1] > 0 && <div style={{ display: 'flex' }}>
+                                <div style={{ background: `hsl(10 100% ${68 - Math.log2(value[1]) * 5}%)`, width: "20px", borderRadius: '90%', padding: '0.2em' }} key={value[0] + idx}>
+                                </div >
+                                {value[0]} : {value[1]}
+                            </div>)
+                    })}
+                </div>
             </div>
-            <div style={{ width: "50%", height: "100%" }}>
-                <Mapkr 
-                    click={(e)=>{console.log(e.target[Object.keys(e.target)[1]].id)}}
-                    locations = {locations}
-                    ref={testRef}
-                />
-            </div>
-            <div style={{display: 'flex',flexDirection:'column',width: "10%",position:'absolute',right:'1em'}}>
-                {Object.entries(locations).map((value,idx) => {
-                    return (
-                    value[0].length > 0 && value[1] > 0 && <div style={{display: 'flex'}}>
-                        <div  style={{background: `hsl(10 100% ${68-Math.log2(value[1])*5}%)`,width:"20px",borderRadius:'90%',padding:'0.2em'}} key={value[0]+idx}>
-                        </div >
-                        {value[0]} : {value[1]}
-                    </div>)
-                })}
-            </div>
+
         </div>
     );
 };
