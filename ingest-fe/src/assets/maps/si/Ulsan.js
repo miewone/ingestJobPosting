d3aad84dafab938c3f7d63acf2f13a072e27b2a4
박원균 <dlsrk489@gmail.com>
2022-11-07 01:32:49 +0900

    [Update] 상세지도들을 사용하기 좋게 변경중입니다.

--- ingest-fe/src/assets/maps/si/Ulsan.js
+++ ingest-fe/src/assets/maps/si/Ulsan.js
@@ -5,43 +5,49 @@ class Ulsan extends React.Component {
     return (
       <svg
         xmlns="http://www.w3.org/2000/svg"
-        width="214"
-        height="214"
+        width="600"
+        height="600"
         overflow="hidden"
 
         viewBox="0 0 1000 1000"
 
       >
         <defs>
-          <clipPath id="clip0">
-            <path d="M2977 1793H3191V2007H2977z"></path>
-          </clipPath>
-        </defs>
-        <g
-          fill="#B8B8B8"
-          fillRule="evenodd"
-          stroke="#FFF"
-          strokeMiterlimit="8"
-          strokeWidth="1.719"
-          clipPath="url(#clip0)"
-          transform="translate(-2977 -1793)"
-        >
-          <path
-            d="M3145.82 1931.1 3144.11 1932.48 3144.79 1936.95 3149.94 1952.07 3149.94 1952.07 3143.08 1979.22 3152 1988.85 3126.94 2005 3109.44 1974.76 3081.29 1974.76 3088.84 1963.76 3079.23 1947.95 3055.2 1943.82 3016.07 1898.45 2989.3 1901.2 2990.33 1892.27 2979 1884.36 2999.94 1871.3 2992.39 1852.05 3011.61 1835.56 3026.71 1839.34 3024.65 1812.53 3054.52 1795 3103.26 1807.03 3102.91 1828.68 3121.11 1837.62 3119.05 1864.77 3121.11 1874.05 3102.57 1866.83 3108.41 1884.71 3095.71 1896.05 3145.82 1931.1Z"
-            stroke="#FFFFFF" stroke-width="1.71893" stroke-miterlimit="8" fill="#B8B8B8" fill-rule="evenodd" />
-          <path
-            d="M3120.73 1836.63 3145.31 1817 3187.29 1831.46 3189 1874.17 3172.27 1881.4 3165.11 1900 3159.64 1896.9 3143.6 1887.26 3143.26 1862.81 3118 1864.18 3120.73 1836.63Z"
-            stroke="#FFFFFF" stroke-width="1.71893" stroke-miterlimit="8" fill="#B8B8B8" fill-rule="evenodd" />
-          <path
-            d="M3189 1874 3186.6 1917.27 3170.16 1928 3169.14 1924.19 3169.14 1920.04 3169.48 1919 3165.71 1910 3164 1902.38 3165.03 1899.27 3172.22 1880.58 3189 1874Z"
-            stroke="#FFFFFF" stroke-width="1.71893" stroke-miterlimit="8" fill="#B8B8B8" fill-rule="evenodd" />
-          <path
-            d="M3107.77 1884 3144.36 1887.42 3157.82 1898.38 3148.85 1898.38 3160.93 1904.88 3162.66 1911.04 3147.81 1909.67 3163 1919.94 3148.85 1935 3146.43 1931.24 3095 1895.3 3107.77 1884Z"
-            stroke="#FFFFFF" stroke-width="1.71893" stroke-miterlimit="8" fill="#B8B8B8" fill-rule="evenodd" />
-          <path d="M3118.39 1864.35 3143.32 1863 3144 1887 3107.8 1883.62 3102 1866.04 3120.44 1873.14Z" stroke="#FFFFFF"
-            stroke-width="1.71893" stroke-miterlimit="8" fill="#B8B8B8" fill-rule="evenodd" />
-
-        </g>
+        <clipPath id="clip0">
+            <rect x="1832" y="873" width="732" height="729" />
+        </clipPath>
+    </defs>
+    <g clipPath="url(#clip0)" transform="translate(-1832 -873)">
+        <path
+            d="M2411.67 1343.63 2405.79 1348.37 2408.13 1363.72 2425.84 1415.63 2425.84 1415.63 2402.25 1508.84 2432.93 1541.91 2346.74 1597.35 2286.55 1493.53 2189.73 1493.53 2215.7 1455.76 2182.65 1401.48 2100 1387.3 1965.42 1231.53 1873.35 1240.97 1876.89 1210.31 1837.93 1183.16 1909.94 1138.32 1883.98 1072.23 1950.08 1015.61 2002.01 1028.59 1994.93 936.54 2097.66 876.354 2265.29 917.657 2264.09 991.989 2326.69 1022.68 2319.6 1115.9 2326.69 1147.76 2262.92 1122.97 2283.01 1184.36 2239.33 1223.29 2411.67 1343.63Z"
+            name="울주군"
+            stroke="#FFFFFF" strokeWidth="5.90889" strokeMiterlimit="8" fill="#B8B8B8" fillRule="evenodd" /><text
+            
+            transform="matrix(1 0 0 1 2042.22 1244)">울주군</text>
+        <path
+            d="M2325.31 1018.99 2409.78 951.351 2554.05 1001.18 2559.93 1148.35 2502.43 1173.26 2477.83 1237.35 2459.03 1226.67 2403.9 1193.45 2402.73 1109.2 2315.93 1113.92 2325.31 1018.99Z"
+            name="북구"
+            stroke="#FFFFFF" strokeWidth="5.90889" strokeMiterlimit="8" fill="#B8B8B8" fillRule="evenodd" /><text
+            
+            transform="matrix(1 0 0 1 2422.06 1102)">북구</text>
+        <path
+            d="M2559.92 1147.35 2551.67 1296.39 2495.11 1333.35 2491.61 1320.23 2491.61 1305.93 2492.78 1302.35 2479.81 1271.35 2473.92 1245.11 2477.47 1234.39 2502.2 1170.02 2559.92 1147.35Z"
+            name="동구"
+            stroke="#FFFFFF" strokeWidth="5.90889" strokeMiterlimit="8" fill="#B8B8B8" fillRule="evenodd" /><text
+            fontFamily="Malgun Gothic,Malgun Gothic_MSFontService,sans-serif" fontWeight="400" fontSize="9"
+            transform="matrix(1 0 0 1 2508 1244)">동구</text>
+        <path
+            d="M2280.87 1182.35 2406.78 1194.09 2453.1 1231.7 2422.23 1231.7 2463.8 1254 2469.75 1275.14 2418.65 1270.44 2470.92 1305.68 2422.23 1357.35 2413.9 1344.45 2236.92 1221.13 2280.87 1182.35Z"
+            name="남구"
+            stroke="#FFFFFF" strokeWidth="5.90889" strokeMiterlimit="8" fill="#B8B8B8" fillRule="evenodd" /><text
+            
+            transform="matrix(1 0 0 1 2317.22 1239)">남구</text>
+        <path d="M2317.12 1114.02 2402.6 1109.35 2404.93 1192.35 2280.81 1180.66 2260.93 1119.86 2324.15 1144.42Z"
+            name="중구"
+            stroke="#FFFFFF" strokeWidth="5.90889" strokeMiterlimit="8" fill="#B8B8B8" fillRule="evenodd" /><text
+            
+            transform="matrix(1 0 0 1 2315.5 1159)">중구</text>
+    </g>
       </svg>
     );
   }
